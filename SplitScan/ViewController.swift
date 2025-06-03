8d6e13bf49dd0292b50ce10f79ab3747f3325d5c
Ri-Hong <riri.hong@gmail.com>
2025-06-03 09:57:50 -0400

    Initial Commit

---
+++ SplitScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitScan
+//
+//  Created by Ri Hong on 2025-06-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SplitScanTests/SplitScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitScanTests.swift
+//  SplitScanTests
+//
+//  Created by Ri Hong on 2025-06-03.
+//
+
+import Testing
+@testable import SplitScan
+
+struct SplitScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
